fc1196aebaf554e62166b37b50c4ec709c9f23ca
Matt Maiorano <maiorano84@gmail.com>
2026-01-20 12:44:15 -0500

    Memcached and APCU adapters added

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ ARG PHP_VERSION=8.4
 FROM wordpress:php${PHP_VERSION}
 
 RUN apt-get update && \
-    apt-get install -y zlib1g-dev libzip-dev unzip && \
-    pecl install redis xdebug && \
-    docker-php-ext-enable xdebug redis;
+    apt-get install -y zlib1g-dev libzip-dev libmemcached-dev unzip && \
+    pecl install redis memcached apcu xdebug && \
+    docker-php-ext-enable redis memcached apcu xdebug;
 
 copy --from=wordpress:cli /usr/local/bin/wp /usr/bin/wp
 COPY --from=composer /usr/bin/composer /usr/bin/composer
--- src/Factory/WPObjectCacheFactory.php
+++ src/Factory/WPObjectCacheFactory.php
@@ -8,6 +8,8 @@ use Maiorano\ObjectCache\Cache\BlogKeyResolver;
 use Maiorano\ObjectCache\Cache\GroupManager;
 use Maiorano\ObjectCache\Cache\Psr6Key;
 use Maiorano\ObjectCache\WPObjectCacheInterface;
+use Symfony\Component\Cache\Adapter\ApcuAdapter;
+use Symfony\Component\Cache\Adapter\MemcachedAdapter;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
 use WP_Object_Cache as WPCache;
 
@@ -35,12 +37,36 @@ final class WPObjectCacheFactory
 
     public static function createAdapterFromConfig(string $adapterType): WPObjectCacheInterface
     {
+        global $wpdb;
+        $defaultNs = self::getConfigOption('WP_OBJECT_CACHE_DEFAULT_NS', $wpdb->prefix);
 
         return match ($adapterType) {
             'redis' => new Psr6(
                 new RedisAdapter(RedisAdapter::createConnection(
                     self::getConfigOption('WP_OBJECT_CACHE_REDIS_DSN', 'redis://localhost:6379')
+                ), $defaultNs),
+                new GroupManager(new BlogKeyResolver(
+                    is_multisite(),
+                    get_current_blog_id(),
+                    '.',
+                    Psr6Key::class,
                 )),
+                new WPObjectCache(new WPCache),
+            ),
+            'memcache', 'memcached' => new Psr6(
+                new MemcachedAdapter(MemcachedAdapter::createConnection(
+                    self::getConfigOption('WP_OBJECT_CACHE_MEMCACHED_DSN', 'memcached://localhost:11211')
+                ), $defaultNs),
+                new GroupManager(new BlogKeyResolver(
+                    is_multisite(),
+                    get_current_blog_id(),
+                    '.',
+                    Psr6Key::class,
+                )),
+                new WPObjectCache(new WPCache),
+            ),
+            'apcu' => new Psr6(
+                new ApcuAdapter($defaultNs),
                 new GroupManager(new BlogKeyResolver(
                     is_multisite(),
                     get_current_blog_id(),
