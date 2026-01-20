cd2d984300c92edff390483cc9439855aa853ed1
Matt Maiorano <maiorano84@gmail.com>
2026-01-20 17:12:03 -0500

    E2E testing on basic PSR-6 adapter functionality

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ FROM wordpress:php${PHP_VERSION}
 RUN apt-get update && \
     apt-get install -y zlib1g-dev libzip-dev libmemcached-dev unzip && \
     pecl install redis memcached apcu xdebug && \
-    docker-php-ext-enable redis memcached apcu xdebug;
+    docker-php-ext-enable redis memcached apcu xdebug && \
+    echo "apc.enable_cli=1" >> /usr/local/etc/php/conf.d/docker-php-ext-apcu.ini
 
 copy --from=wordpress:cli /usr/local/bin/wp /usr/bin/wp
 COPY --from=composer /usr/bin/composer /usr/bin/composer
--- tests/E2E/Factory/WPObjectCacheFactoryTest.php
+++ tests/E2E/Factory/WPObjectCacheFactoryTest.php
@@ -5,9 +5,14 @@ namespace E2E\Factory;
 use Maiorano\ObjectCache\Adapter\Psr6;
 use Maiorano\ObjectCache\Adapter\WPObjectCache;
 use Maiorano\ObjectCache\Cache\BlogKeyResolver;
+use Maiorano\ObjectCache\Cache\Group;
 use Maiorano\ObjectCache\Cache\GroupManager;
+use Maiorano\ObjectCache\Cache\Key;
+use Maiorano\ObjectCache\Cache\Psr6Key;
 use Maiorano\ObjectCache\Factory\WPObjectCacheFactory;
+use Maiorano\ObjectCache\WPObjectCacheInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
 use PHPUnit\Framework\TestCase;
 
@@ -16,6 +21,9 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(WPObjectCache::class)]
 #[CoversClass(BlogKeyResolver::class)]
 #[CoversClass(GroupManager::class)]
+#[CoversClass(Group::class)]
+#[CoversClass(Key::class)]
+#[CoversClass(Psr6Key::class)]
 class WPObjectCacheFactoryTest extends TestCase
 {
     public function testCreateWordpressAdapterFromConfig(): void
@@ -47,5 +55,28 @@ class WPObjectCacheFactoryTest extends TestCase
     {
         define('WP_OBJECT_CACHE_DEFAULT_NS_ENABLED', false);
         WPObjectCacheFactory::createAdapterFromConfig('wordpress');
+    }
+
+    #[DataProvider('adapterDataProvider')]
+    public function testAdapters(WPObjectCacheInterface $adapter): void
+    {
+        $this->assertFalse($adapter->get('non-existent', 'phpunit'));
+        $this->assertTrue($adapter->set('non-existent', 'exists', 'phpunit'));
+
+        $newValue = $adapter->get('non-existent', 'phpunit', false, $isHit);
+        $this->assertEquals('exists', $newValue);
+        $this->assertTrue($isHit);
+
+        $this->assertTrue($adapter->delete('non-existent', 'phpunit'));
+        $this->assertFalse($adapter->get('non-existent', 'phpunit'));
+    }
+
+    public static function adapterDataProvider(): array
+    {
+        return [
+            [WPObjectCacheFactory::createAdapterFromConfig('redis')],
+            [WPObjectCacheFactory::createAdapterFromConfig('memcached')],
+            [WPObjectCacheFactory::createAdapterFromConfig('apcu')],
+        ];
     }
 }
